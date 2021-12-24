94243456073294793092dde5ea1c803453b69d34
sapanju <sapanju313@gmail.com>
2021-12-24 11:32:21 -0500

    move middleware to own file, fix admin authenticator

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,55 +2,33 @@ const todosController = require('../controllers').todos;
 const todoItemsController = require('../controllers').todoItems;
 const projectsController = require('../controllers').projectsController;
 const usersController = require('../controllers').usersController;
-const User = require('../models').User;
-
-const jwt = require('jsonwebtoken');
-
-
-async function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-
-  if (token == null) return res.sendStatus(401)
-
-  try {
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);  
-    const user = await User.findByPk(decoded.userId);
-    req.user = user;
-    next();
-  }
-  catch (err) {
-    console.log(err);
-    return res.sendStatus(403);
-  }
-}
+const authenticateToken = require('./middleware').authenticateToken;
+const authenticateAdmin = require('./middleware').authenticateAdmin;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Todos API!',
   }));
 
-  app.post('/api/todos', todosController.create);
-  app.get('/api/todos', todosController.list);
-  app.get('/api/todos/:todoId', todosController.retrieve);
-  app.put('/api/todos/:todoId', todosController.update);
-  app.delete('/api/todos/:todoId', todosController.destroy);
+  app.post('/api/todos', authenticateToken, todosController.create);
+  app.get('/api/todos', authenticateToken, todosController.list);
+  app.get('/api/todos/:todoId', authenticateToken, todosController.retrieve);
+  app.put('/api/todos/:todoId', authenticateToken, todosController.update);
+  app.delete('/api/todos/:todoId', authenticateToken, todosController.destroy);
 
-  app.post('/api/todos/:todoId/items', todoItemsController.create);
-  app.put('/api/todos/:todoId/items/:todoItemId', todoItemsController.update);
+  app.post('/api/todos/:todoId/items', authenticateToken, todoItemsController.create);
+  app.put('/api/todos/:todoId/items/:todoItemId', authenticateToken, todoItemsController.update);
   app.delete(
-    '/api/todos/:todoId/items/:todoItemId', todoItemsController.destroy
+    '/api/todos/:todoId/items/:todoItemId', authenticateToken, todoItemsController.destroy
   );
   app.all('/api/todos/:todoId/items', (req, res) => res.status(405).send({
     message: 'Method Not Allowed',
   }));
 
-  app.post('/api/projects', projectsController.create);
-  app.post('/api/projects/:projectId/users', projectsController.addUserToProject);
+  app.post('/api/projects', authenticateToken, projectsController.create);
+  app.post('/api/projects/:projectId/users', authenticateToken, projectsController.addUserToProject);
 
   app.get('/api/users', authenticateToken, usersController.getUser); // dashboard endpoint
   app.post('/api/users', usersController.register);
   app.post('/login', usersController.login);
-  // app.post('/logout', usersController.logout);
-
 };
